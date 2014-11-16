fc3bcdad6ecf39e37d92f03983cd40f2c3603568
jls9fc <jls9fc@virginia.edu>
2014-11-15 23:39:48 -0500

    Bulletins save in DB now
    That is all

--- Secure_Witness_App/models.py
+++ Secure_Witness_App/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+# TODO fix data types later amd match the form input on html page.
+# class Bulletin(models.Model):
+#     Author_ID = models.TextField #    (max_length=30)
+#     Date = models.DateField()  # is Date, Month, Year.  (2002, 12, 31)
+#     Address = models.TextField
+#     Post_Code = models.IntegerField(max_length=10) #I think max zip is 10.  "zip+4"
+#     Country = models.CharField()
+#     Description = models.TextField
+
+class Bulletin(models.Model):
+    Author_ID = models.TextField() #    (max_length=30)
+    Date = models.TextField()  # is Date, Month, Year.  (2002, 12, 31)
+    Address = models.TextField()
+    Post_Code = models.TextField() #I think max zip is 10.  "zip+4"
+    Country = models.TextField()
+    Description = models.TextField()
