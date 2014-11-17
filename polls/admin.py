6cd018649c6ef19eb035f5a9873b8c70470ccbb9
jls9fc <jls9fc@virginia.edu>
2014-11-16 19:27:33 -0500

    clear
    clear

--- polls/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-#the urls created from where ever are found here.
-#   http://127.0.0.1:8000/admin/auth/
-#  http://127.0.0.1:8000/polls/
