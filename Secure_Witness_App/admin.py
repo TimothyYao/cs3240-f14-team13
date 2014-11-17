5ad56f0f8aa01313c404a898b455e4a4d22094b4
jls9fc <jls9fc@virginia.edu>
2014-11-17 12:13:39 -0500

    fixed conflicts
    dsads

--- Secure_Witness_App/admin.py
+++ Secure_Witness_App/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 from Secure_Witness_App.models import Bulletin
 # Register your models here.
-<<<<<<< HEAD
 
-=======
->>>>>>> ec83ea23fb14c2c57f4ca4f8e2101432a2fe7327
 admin.site.register(Bulletin)
