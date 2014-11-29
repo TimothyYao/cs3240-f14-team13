f3939253188db4d5bef5355fb9255288b968a80b
Tim Yao <ly3py@virginia.edu>
2014-11-28 20:05:29 -0500

    added bulletin editing

--- Current Project/SecureWitness_1_0/bulletins/views.py
+++ Current Project/SecureWitness_1_0/bulletins/views.py
@@ -20,11 +20,11 @@ def index(request):
         'user': request.user
     })
 
+@login_required()
 def search(request):
     if len(request.GET) > 0:
         term = request.GET['search']
         category = request.GET['type']
-        print "there was a GET request. . ."
         bulletins = Bulletin.objects.search(term, category)[:50]
         return render(request, 'search_results.html', {
             'bulletins': bulletins,
@@ -32,18 +32,33 @@ def search(request):
         })
     return render(request, 'search.html')
 
+@login_required()
+def search_redirect(request, bulletin_id):
+    return HttpResponseRedirect('/'+bulletin_id+'/')
+
+@login_required()
 def details(request, bulletin_id):
+    if request.method == 'POST':
+        if 'edit' in request.POST:
+            return HttpResponseRedirect('/'+bulletin_id+'/edit/')
+        elif 'delete' in request.POST:
+            Bulletin.objects.get(pk=bulletin_id).delete()
+            return HttpResponseRedirect('/')
+        elif 'move' in request.POST:
+            print 'move'
     bulletin = Bulletin.objects.get(pk=bulletin_id)
-    return render(request, 'details.html', {'bulletin': bulletin})
+    owner = request.user == bulletin.Author
+    return render(request, 'details.html', {
+        'bulletin': bulletin,
+        'owner': owner
+    })
 
 def register(request):
     message = 'Create User'
     if request.method == 'POST':
-        name = request.POST ['username']
-        pw = request.POST ['password']
-        pw2 = request.POST ['password2']
-        #all_users = list(Users.objects);
-        #print all_users;
+        name = request.POST['username']
+        pw = request.POST['password']
+        pw2 = request.POST['password2']
         if pw == pw2:
             if len(pw) < 7:
                 message = 'password must have at least 7 characters'
@@ -73,3 +88,20 @@ def submit(request):
     else:
         form = BulletinForm()
     return render(request, 'submit.html', {'form': form})
+
+@login_required()
+def edit_bulletin(request, bulletin_id):
+    bulletin = Bulletin.objects.get(pk=bulletin_id)
+    if request.method == 'POST':
+        print request.POST
+        if 'cancel' in request.POST:
+            return HttpResponseRedirect('/'+bulletin_id+'/')
+        bulletin.Title = request.POST["title"]
+        bulletin.Pseudonym = request.POST["pseudonym"]
+        bulletin.Location = request.POST["location"]
+        bulletin.Description = request.POST["description"]
+        bulletin.save()
+        return HttpResponseRedirect('/'+bulletin_id+'/')
+    return render(request, 'edit_bulletin.html', {
+        'bulletin': bulletin
+    })
