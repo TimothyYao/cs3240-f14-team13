d9b9e4c65d0cbeda9d9eece5a0fa174a86713a14
Tim Yao <ly3py@virginia.edu>
2014-12-03 20:32:14 -0500

    saved

--- bulletins/views.py
+++ bulletins/views.py
@@ -13,6 +13,95 @@ from models import Bulletin, BulletinSearch, File, Folder, Permission  #TODO WAS
 #from bulletins.models import Bulletin, Folder, File, Permission
 
 
+from Crypto.Hash import SHA256
+import os, random, struct
+from Crypto.Cipher import AES
+
+
+def getKey(password):
+    hasher = SHA256.new(password)
+    return hasher.digest()
+
+
+
+def encrypt_file(key, in_filename, out_filename=None, chunksize=64*1024):
+    """ Encrypts a file using AES (CBC mode) with the
+        given key.
+
+        key:
+            The encryption key - a string that must be
+            either 16, 24 or 32 bytes long. Longer keys
+            are more secure.
+
+        in_filename:
+            Name of the input file
+
+        out_filename:
+            If None, '<in_filename>.enc' will be used.
+
+        chunksize:
+            Sets the size of the chunk which the function
+            uses to read and encrypt the file. Larger chunk
+            sizes can be faster for some files and machines.
+            chunksize must be divisible by 16.
+    """
+    if not out_filename:
+        out_filename = in_filename + '.enc'
+
+    iv = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
+    encryptor = AES.new(key, AES.MODE_CBC, iv)
+    filesize = os.path.getsize(in_filename)
+
+    with open(in_filename, 'rb') as infile:
+        with open(out_filename, 'wb') as outfile:
+            outfile.write(struct.pack('<Q', filesize))
+            outfile.write(iv)
+
+            while True:
+                chunk = infile.read(chunksize)
+                if len(chunk) == 0:
+                    break
+                elif len(chunk) % 16 != 0:
+                    chunk += ' ' * (16 - len(chunk) % 16)
+
+                outfile.write(encryptor.encrypt(chunk))
+
+
+
+
+def decrypt_file(key, in_filename, out_filename=None, chunksize=24*1024):
+    """ Decrypts a file using AES (CBC mode) with the
+        given key. Parameters are similar to encrypt_file,
+        with one difference: out_filename, if not supplied
+        will be in_filename without its last extension
+        (i.e. if in_filename is 'aaa.zip.enc' then
+        out_filename will be 'aaa.zip')
+    """
+    if not out_filename:
+        out_filename = os.path.splitext(in_filename)[0]
+
+    with open(in_filename, 'rb') as infile:
+        origsize = struct.unpack('<Q', infile.read(struct.calcsize('Q')))[0]
+        iv = infile.read(16)
+        decryptor = AES.new(key, AES.MODE_CBC, iv)
+
+        with open(out_filename, 'wb') as outfile:
+            while True:
+                chunk = infile.read(chunksize)
+                if len(chunk) == 0:
+                    break
+                outfile.write(decryptor.decrypt(chunk))
+
+            outfile.truncate(origsize)
+
+
+
+
+
+
+
+
+
 def recent_bulletins(recent=100):
     latest_bulletins = Bulletin.objects.all().order_by('-Date')[:recent]
     return latest_bulletins
@@ -58,6 +147,13 @@ def handle_upload(request, bulletin):
                 upload.File_Field = i
                 #TODO encrypt here
                 upload.save()
+
+
+                key = getKey("some password") #hash+pad key for 16,32,64?   TODO Do something else with KEY ! ! !
+                fileToEncrypt = upload.File_Field.path
+                encryptedOutput = upload.File_Field.name
+                encrypt_file(key, fileToEncrypt, encryptedOutput)   #Moved this to after Save. . .
+
 
 def index(request):
     bulletins = recent_bulletins(recent=50)
